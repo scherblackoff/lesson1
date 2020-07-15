39f2a5934f4a8fec55f92fad2d812acae5c98971
scherblackoff <scherblackoff@gmail.com>
2020-07-15 21:57:36 +0300

    homework

---
+++ src/lesson_1/Apple.java
@@ -0,0 +1,10 @@
+package lesson_1;
+
+public class Apple extends Fruit {
+    private double weight = 1;
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+}
---
+++ src/lesson_1/Orange.java
@@ -0,0 +1,10 @@
+package lesson_1;
+
+public class Orange extends Fruit {
+    private double weight = 1.5;
+
+    @Override
+    public double getWeight() {
+        return this.weight;
+    }
+}
